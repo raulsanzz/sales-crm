ace0066607c7262ab73c8df15a203a20fa6e9a71
Arslan Ansari <arslanmehmood18@gmail.com>
2020-03-06 00:03:41 +0500

    run frontend through backend

--- server/server.js
+++ server/server.js
@@ -2,15 +2,19 @@ const path = require("path");
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 5000;
 const db = require("./database/db");
 var cors = require("cors");
-
+const reactBuildPath = path.join(__dirname, "../build")
+app.use(express.static(reactBuildPath));
 app.use(express.json({ extended: false }));
 app.use(cors());
+
 var ip = require("ip");
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(reactBuildPath, 'index.html'));
+});
 app.use("/api/user", require("./routes/user"));
 app.use("/api/job", require("./routes/job"));
 app.use("/api/auth", require("./routes/auth"));
