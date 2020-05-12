43dc9d651e5072d5ceedff391a760b1000d6ff0f
jalal <jaalk47@gmail.com>
2020-05-13 00:24:42 +0500

    ReportsAll

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,5 +1,10 @@
 const db = require("../database/db");
+const express = require("express");
+const auth = require("../middleware/auth");
+const Router = express.Router();
 const Note = db.note;
+const sequelize = db.Sequelize;
+const Op = sequelize.Op;
 
 //Add a new note for an Agenda
 const addNote = async(newNote) => {
@@ -13,6 +18,33 @@ const addNote = async(newNote) => {
         throw new Error ({ msg: "Server Error" });
     }
 }
-module.exports = {
-    addNote
-};
+
+//Get all notes with respect to names
+Router.put( "/voiceReport", auth, async (req, res) => {
+    try {
+    const voiceReport = await Note.findAll({
+      where: {
+        createdAt:{
+              [Op.and]: {
+                [Op.gte]: req.body.startDate,
+                [Op.lte]: req.body.endDate
+              } }
+  
+            },
+            attributes: ['voice', [sequelize.fn('COUNT', sequelize.col('voice')), 'total']],
+        group: ['voice'],
+      })
+  
+      
+      return res.json({voiceReport});
+    } catch (error) {
+      console.log('====================================');
+      console.log(error);
+      console.log('====================================');
+      // console.log(error.message);
+      return res.status(402).json({ msg: "Server Error" });
+    }
+  });
+
+module.exports = Router,{addNote}
+
--- server/routes/tests.js
+++ server/routes/tests.js
@@ -2,8 +2,9 @@ const express = require("express");
 const Router = express.Router();
 const auth = require("../middleware/auth");
 const db = require("../database/db");
+const sequelize = db.Sequelize;
 const Test = db.test;
-
+const Op = sequelize.Op;
 //Add a new Test
 Router.post("/", auth, async(req, res) => {
     try {
@@ -19,8 +20,36 @@ Router.post("/", auth, async(req, res) => {
     }
 })
 
+
+//get all test statuses respect to pass no response etc.
+Router.put( "/testReport", auth, async (req, res) => {
+  try {
+  const testReport = await Test.findAll({
+    where: {
+      createdAt:{
+            [Op.and]: {
+              [Op.gte]: req.body.startDate,
+              [Op.lte]: req.body.endDate
+            } }
+
+          },
+          attributes: ['status', [sequelize.fn('COUNT', sequelize.col('status')), 'total']],
+      group: ['status'],
+    })
+
+    
+    return res.json({testReport});
+  } catch (error) {
+    console.log('====================================');
+    console.log(error);
+    console.log('====================================');
+    // console.log(error.message);
+    return res.status(402).json({ msg: "Server Error" });
+  }
+});
+
 //get all tests
-Router.put( "/:id", auth, async (req, res) => {
+Router.put( "/:id", async (req, res) => {
     try {
       const test = await Test.update({
         ...req.body.test
@@ -32,5 +61,6 @@ Router.put( "/:id", auth, async (req, res) => {
       return res.status(402).json({ msg: "Server Error" });
     }
 });
+
 
 module.exports = Router;
